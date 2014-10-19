1bff8811e346ffad31446890b0d58c916778b290
Jalil Kazemitabar <sjalilkazemitabar@gmail.com>
2014-10-19 15:09:46 -0700

    An entity to store information for a news item.
    It matches the tag <item> in rss.

---
+++ src/com/jalil/environ/rss/Item.java
@@ -0,0 +1,56 @@
+package com.jalil.environ.rss;
+
+import java.net.URL;
+
+public class Item {
+	
+	private final String title;
+	private final URL link;
+	private final String description;
+	
+	public Item(String title, URL link, String description) {
+		this.title = title;
+		this.link = link;
+		this.description = description;
+	}
+
+	public String getTitle() {
+	    return title;
+    }
+
+	public URL getLink() {
+	    return link;
+    }
+
+	public String getDescription() {
+	    return description;
+    }
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || !(o instanceof Item))
+			return false;
+		
+		Item that = (Item) o;
+		
+		if (!title.equals(that.title))
+			return false;
+		if (!link.equals(that.link))
+			return false;
+		if(description != null ? !description.equals(that.description) : that.description != null)
+			return false;
+		
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = title.hashCode();
+		result = 31 * result + link.hashCode();
+		result = 31 * result + (description != null ? description.hashCode() : 0);
+		return result;
+	}
+
+}
