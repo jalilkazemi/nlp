9b3da8b8c990b4597a0a84fd20ca690f7d3000ea
Jalil Kazemitabar <sjalilkazemitabar@gmail.com>
2014-10-19 15:28:55 -0700

    Create a table to store migration meta data.
    Each row in migration_meta_data corresponds to an update to the database.
    It keeps track of old migrations and help avoid duplicate updates.

--- src/com/jalil/environ/repository/Migrator.java
+++ src/com/jalil/environ/repository/Migrator.java
@@ -1,25 +1,19 @@
 package com.jalil.environ.repository;
 
 import java.io.File;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.sql.Date;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 import com.google.common.base.Charsets;
-import com.google.common.io.CharStreams;
 import com.google.common.io.Files;
 
 
@@ -28,11 +22,25 @@ public class Migrator {
 	private final File migrationDirectory;
 	private final Connection con;
 	
+	private String migrationPrefixFormat = "yyyy-MM-dd hh-mm-ss";
+	private static String CREATE_META_DATA = "CREATE TABLE IF NOT EXISTS migration_meta_data( " + 
+	 										 " id INTEGER PRIMARY KEY NOT NULL, " + 
+	 										 " migration_datetime DATETIME NOT NULL)";
+
 	public Migrator(File migrationDirectory, Connection con) {
 		this.migrationDirectory = migrationDirectory;
 		this.con = con;
 	}
 	
 	public void applyUpdates() throws Exception {
+		initializeMigrationMetaData();
 	}
+	
+	private void initializeMigrationMetaData() throws Exception {
+		Statement createStmt = con.createStatement();
+		try {
+			createStmt.executeUpdate(CREATE_META_DATA);	
+		} finally { createStmt.close(); }
+	}
+	
 }
