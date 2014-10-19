13d48dc0b1307e0133e95245e86df685c35bb6b0
Jalil Kazemitabar <sjalilkazemitabar@gmail.com>
2014-10-19 15:24:11 -0700

    The server program.
    It will initiate all other pieces and pass appropirate configurations to them.

---
+++ src/com/jalil/environ/server/Run.java
@@ -0,0 +1,13 @@
+package com.jalil.environ.server;
+
+import java.sql.Connection;
+
+import static com.jalil.environ.Configuration.*;
+import com.jalil.environ.repository.Migrator;
+import com.jalil.environ.repository.SQLiteJDBC;
+
+public class Run {
+	
+	public static void main(String[] args) {
+	}
+}
