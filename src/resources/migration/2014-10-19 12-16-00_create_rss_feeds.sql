fcf94da337c9e5f5548f6121b325413840b9abff
Jalil Kazemitabar <sjalilkazemitabar@gmail.com>
2014-10-19 17:05:26 -0700

    Create tables for rss feeds (news feeds).
    channels: media outlets, news blogs, etc.
    items: posts
    fetches: the content of a post, fetched at a certain time by the program
    
    channels and items are summeries found in a rss xml. The content can be found
    in the links provided in the rss.

---
+++ src/resources/migration/2014-10-19 12-16-00_create_rss_feeds.sql
@@ -0,0 +1,26 @@
+CREATE TABLE channels(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(80) NOT NULL,
+    link VARCHAR(80) UNIQUE NOT NULL,
+    description VARCHAR(500),
+    language CHAR(2)    
+);
+
+CREATE TABLE items(
+    id INTEGER PRIMARY KEY,
+    channel_pk INTEGER NOT NULL,
+    title VARCHAR(80) NOT NULL,
+    link VARCHAR(80) UNIQUE NOT NULL,
+    description VARCHAR(500)
+);
+
+CREATE INDEX idx_items_channel ON items(channel_pk);
+
+CREATE TABLE fetches(
+    id INTEGER PRIMARY KEY,
+    item_pk INTEGER NOT NULL,
+    body TEXT NOT NULL,
+    fetched_datetime DATETIME NOT NULL
+);
+
+CREATE INDEX idx_fetchs_item_datetime ON fetchs(item_pk, fetched_datetime);
