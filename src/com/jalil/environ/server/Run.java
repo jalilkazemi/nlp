c47dada25095e1868f855dbef24a0c4eaa33e9e0
Jalil Kazemitabar <sjalilkazemitabar@gmail.com>
2014-11-16 12:47:43 -0800

    Run: collect new posts from world wide web.

--- src/com/jalil/environ/server/Run.java
+++ src/com/jalil/environ/server/Run.java
@@ -2,17 +2,34 @@ package com.jalil.environ.server;
 
 import java.sql.Connection;
 
+import javax.xml.bind.JAXBException;
+
 import static com.jalil.environ.Configuration.*;
+
+import com.jalil.environ.fetch.PostFetcher;
+import com.jalil.environ.fetch.RssFetcher;
 import com.jalil.environ.repository.Migrator;
+import com.jalil.environ.repository.PostDao;
+import com.jalil.environ.repository.RssFeedDao;
 import com.jalil.environ.repository.SQLiteJDBC;
+import com.jalil.environ.worker.NewsCollector;
 
 public class Run {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JAXBException {
 		Connection con = SQLiteJDBC.getConnection();
 		Migrator migrator = new Migrator(migrationDirectory, con);
+		
+		RssFeedDao rssDao = new RssFeedDao(con);
+		PostDao postDao = new PostDao(con);
+		RssFetcher rssFetcher = new RssFetcher();
+		PostFetcher postFetcher = new PostFetcher();
+		
+		NewsCollector newsCollector = new NewsCollector(rssFetcher, postFetcher, rssDao, postDao);
+		
 		try {
 			migrator.applyUpdates();
+			newsCollector.collect();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
