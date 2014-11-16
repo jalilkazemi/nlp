cfb5b6b860739faadbf131c49e78dda2ba5eab35
Jalil Kazemitabar <sjalilkazemitabar@gmail.com>
2014-11-16 15:08:44 -0800

    PostFetcherTest: test PostFetcher without connecting to internet.

--- src/com/jalil/environ/fetch/test/PostFetcherTest.java
+++ src/com/jalil/environ/fetch/test/PostFetcherTest.java
@@ -1,6 +1,11 @@
 package com.jalil.environ.fetch.test;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.MalformedURLException;
 
 import javax.xml.bind.JAXBException;
 
@@ -9,13 +14,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import com.jalil.environ.fetch.AddrToReader;
 import com.jalil.environ.fetch.PostFetcher;
 import com.jalil.environ.html.Post;
 
 @RunWith(JUnit4.class)
 public class PostFetcherTest {
 	
-	private static String rssAddr = "http://www.iew.ir/1393/08/24/30843";
+	private static String postStr = "<html><head></head><body><div class=\"entry-meta\">meta</div><div class=\"entry-content\">body</div></body></html>";
 	
 	@BeforeClass
 	public static void init() throws IOException {
@@ -23,8 +29,16 @@ public class PostFetcherTest {
 
 	@Test
 	public void testXmlParser() throws IOException, JAXBException {
-		PostFetcher fetcher = new PostFetcher();
-		Post post = fetcher.fetch(rssAddr);
-		System.out.println(post);
+		PostFetcher fetcher = new PostFetcher(new AddrToReader() {
+
+			@Override
+            public Reader reader(String addr) throws MalformedURLException, IOException {
+	            return new StringReader(postStr);
+            }
+			
+		});
+		Post post = fetcher.fetch("");
+		assertEquals("meta", post.getMeta());
+		assertEquals("body", post.getBody());
 	}
 }
