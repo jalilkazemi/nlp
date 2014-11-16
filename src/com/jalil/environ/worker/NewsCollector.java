f989c3a16d1e2972c8cd968d651e92aca5a8248c
Jalil Kazemitabar <sjalilkazemitabar@gmail.com>
2014-11-16 12:33:41 -0800

    NewsCollector: collect all new posts and store them
    1. Reads rss_pages for the list of rss resources
    2. download all rss feeds and store the feeds (items)
    3. chech if each item is already fetched
    4. download and store posts that have not been fetched before

---
+++ src/com/jalil/environ/worker/NewsCollector.java
@@ -0,0 +1,42 @@
+package com.jalil.environ.worker;
+
+import java.util.Set;
+import com.jalil.environ.fetch.PostFetcher;
+import com.jalil.environ.fetch.RssFetcher;
+import com.jalil.environ.repository.PostDao;
+import com.jalil.environ.repository.RssFeedDao;
+import com.jalil.environ.rss.Item;
+import com.jalil.environ.rss.RssFeed;
+
+public class NewsCollector {
+	
+	private final RssFetcher rssFetcher;
+	private final PostFetcher postFetcher;
+	private final RssFeedDao rssDao;
+	private final PostDao postDao;
+	
+	public NewsCollector(RssFetcher rssFetcher, PostFetcher postFetcher,
+						RssFeedDao rssDao, PostDao postDao) {
+		this.rssFetcher = rssFetcher;
+		this.postFetcher = postFetcher;
+		this.rssDao = rssDao;
+		this.postDao = postDao;
+	}
+	
+	public void collect() {
+		try {
+	        Set<String> rssPages = rssDao.restoreRssPages();
+	        for (String rssPage : rssPages) {
+	        	RssFeed rss = rssFetcher.fetch(rssPage);
+	        	rssDao.storeRssFeed(rss);
+	        	for (Item item : rss.getChannel().getItems()) {
+	        		if (postDao.restorePost(item).isEmpty()) {
+	        			postDao.storePost(item, postFetcher.fetch(item.getLink()));
+	        		}
+	        	}
+	        }
+        } catch (Exception e) {
+	        System.out.println("Failed to collect news: " + e.getMessage());
+        }
+	}
+}
