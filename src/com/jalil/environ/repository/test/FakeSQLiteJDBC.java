3b734bae0b501fb3b1fab7c36de71d0ddf252b19
Jalil Kazemitabar <sjalilkazemitabar@gmail.com>
2015-01-01 17:24:28 -0800

    FakeSQLiteJDBC: clean up (after each test)

--- src/com/jalil/environ/repository/test/FakeSQLiteJDBC.java
+++ src/com/jalil/environ/repository/test/FakeSQLiteJDBC.java
@@ -2,7 +2,9 @@ package com.jalil.environ.repository.test;
 
 import static com.jalil.environ.Configuration.*;
 
+import java.io.File;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,21 +12,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class FakeSQLiteJDBC {
+	
 	private static Connection con = null;
 	
 	static {
 	    try {
 	        Class.forName("org.sqlite.JDBC");
 	        con = DriverManager.getConnection("jdbc:sqlite:" + testDatabaseName);
-	        Statement stmt = con.createStatement();
-	        ResultSet rs = stmt.executeQuery("SELECT name FROM sqlite_master WHERE type = 'table'");
-	        List<String> tables = new LinkedList<String>();
-	        while (rs.next()) {
-	        	tables.add(rs.getString("name"));
-	        }
-	        for (String table : tables) {
-	        	stmt.executeUpdate("DROP TABLE " + table);	        	
-	        }
 	    } catch (Exception e) {
 	        e.printStackTrace();
 	        System.exit(1);
@@ -33,5 +27,17 @@ public class FakeSQLiteJDBC {
 	
 	public static Connection getConnection() {
 		return con;
+	}
+	
+	public static void cleanUp() throws SQLException {
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT name FROM sqlite_master WHERE type = 'table'");
+        List<String> tables = new LinkedList<String>();
+        while (rs.next()) {
+        	tables.add(rs.getString("name"));
+        }
+        for (String table : tables) {
+        	stmt.executeUpdate("DROP TABLE " + table);	        	
+        }
 	}
 }
